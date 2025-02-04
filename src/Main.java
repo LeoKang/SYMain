847f3edcfe70ab1383422d5488ab913b110da353
SYU <xeungjin@gmail.com>
2025-02-04 12:28:18 +0900

    test branch commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ import java.text.DecimalFormat;
 public class Main {
     public static void main(String[] args) {
        System.out.println("test branch");
+        System.out.println("local test branch");
     }
 }
